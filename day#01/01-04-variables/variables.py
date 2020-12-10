562ebd7bf71cb77e114727d454410f2a1b0e5d39
danflint <tumblerlogic@gmail.com>
2020-12-10 18:51:03 +0100

    switch the values in variables

---
+++ day#01/01-04-variables/variables.py
@@ -0,0 +1,8 @@
+#Write a program that switches the values stored in the variables a and b.
+
+a = input("type a")
+b = input("type b")
+new_a = b
+new_b = a
+print(f"a: {new_a}")
+print(f"b: {new_b}")
