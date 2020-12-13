70d85f4b17d5cf8d260fd4d89edd33d24225ddca
danflint <tumblerlogic@gmail.com>
2020-12-13 19:31:17 +0100

    Add two sings new line that improve readable program.

--- day#04/04-02-who-pays-the-bill/who_pays.py
+++ day#04/04-02-who-pays-the-bill/who_pays.py
@@ -4,11 +4,11 @@ The rule is simple who has been chose chosen pay for all.
 """
 from random import randint
 
-participants_quantity = int(input("How many people play?"))
+participants_quantity = int(input("How many people play?\n"))
 participants_lists  = []
 
 for i in range(participants_quantity):
-    participants_lists.append(input("Type the full name who is playing."))
+    participants_lists.append(input("Type the full name who is playing.\n"))
 
 payer = participants_lists[randint(0, participants_quantity - 1)]
 
