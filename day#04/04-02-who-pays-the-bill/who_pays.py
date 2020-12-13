67b82bb3616f110eed8dfd3b609f052da0593aaa
danflint <tumblerlogic@gmail.com>
2020-12-13 19:26:42 +0100

    Draw who will pay the bill.

---
+++ day#04/04-02-who-pays-the-bill/who_pays.py
@@ -0,0 +1,15 @@
+"""
+Type the names and draw who pays the bill?
+The rule is simple who has been chose chosen pay for all.
+"""
+from random import randint
+
+participants_quantity = int(input("How many people play?"))
+participants_lists  = []
+
+for i in range(participants_quantity):
+    participants_lists.append(input("Type the full name who is playing."))
+
+payer = participants_lists[randint(0, participants_quantity - 1)]
+
+print(f"{payer.capitalize()} pays the bill.")
