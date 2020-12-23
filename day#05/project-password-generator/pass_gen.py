63d3f42920a901fb06d2df2c35d41a0a30c70ca6
danflint <tumblerlogic@gmail.com>
2020-12-23 22:56:50 +0100

    Generate random password.

---
+++ day#05/project-password-generator/pass_gen.py
@@ -0,0 +1,26 @@
+"""
+PyPassword Generator.
+Password can be created with letters, numbers and special symbols.
+"""
+from random import randint, shuffle
+
+letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numbers = '0123456789'
+symbols = '!@#$%^&*()_+}{'
+letters_num = input("How many letters would you like in your passwrod?")
+numbers_num = input("How many numbers would you like in your passwrod?")
+symbols_num = input("How many special symbols would you like in your passwrod?")
+password = []
+
+for i in range(int(letters_num)):
+    password.append(letters[randint(0, len(letters) - 1)])
+for i in range(int(numbers_num)):
+    password.append(numbers[randint(0, len(numbers) - 1)])
+for i in range(int(symbols_num)):
+    password.append(symbols[randint(0, len(symbols) - 1)])
+
+shuffle(password)
+print(password)
+pass_str = ''.join(str(sign) for sign in password)
+
+print(f"Here is your password: {pass_str}")
